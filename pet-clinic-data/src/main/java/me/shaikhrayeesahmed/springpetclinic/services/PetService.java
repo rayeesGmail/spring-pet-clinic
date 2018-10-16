248517e611796b61e2126e8573fce61bb090e2d8
rayees <shaikhrayeesahmed@gmail.com>
2018-10-17 02:37:12 +0530

    Adding base service interface. Closes #11.

--- pet-clinic-data/src/main/java/me/shaikhrayeesahmed/springpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/me/shaikhrayeesahmed/springpetclinic/services/PetService.java
@@ -2,15 +2,8 @@ package me.shaikhrayeesahmed.springpetclinic.services;
 
 import me.shaikhrayeesahmed.springpetclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 
 
 }
