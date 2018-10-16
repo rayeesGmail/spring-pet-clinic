425995738d6bf4cf75ce7b2a33e470ed46bf23d7
rayees <shaikhrayeesahmed@gmail.com>
2018-10-16 10:44:51 +0530

    adding POJOs. Closes #2.

---
+++ src/main/java/me/shaikhrayeesahmed/springpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package me.shaikhrayeesahmed.springpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/me/shaikhrayeesahmed/springpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package me.shaikhrayeesahmed.springpetclinic.model;
+
+public class Vet extends Person {
+}
