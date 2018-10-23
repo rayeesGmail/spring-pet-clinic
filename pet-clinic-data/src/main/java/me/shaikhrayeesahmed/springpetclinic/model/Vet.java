fa27502c2001b3f2840dc23f2a7e6a1743126eba
rayees <shaikhrayeesahmed@gmail.com>
2018-10-23 13:20:31 +0530

    Create Vet Speciality Entity and associate to vet. closes #28

--- pet-clinic-data/src/main/java/me/shaikhrayeesahmed/springpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/me/shaikhrayeesahmed/springpetclinic/model/Vet.java
@@ -1,4 +1,17 @@
 package me.shaikhrayeesahmed.springpetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
+
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
+
 }
