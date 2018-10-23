6fe0f267f9bc30243b648bd7b954cb38ab041c7b
rayees <shaikhrayeesahmed@gmail.com>
2018-10-23 13:24:56 +0530

    Add Contact info to owner. closes #29

--- pet-clinic-data/src/main/java/me/shaikhrayeesahmed/springpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/me/shaikhrayeesahmed/springpetclinic/model/Owner.java
@@ -4,7 +4,34 @@ import java.util.Set;
 
 public class Owner extends Person {
 
-    Set<Pet> pets;
+    private String addrress;
+    private String city;
+    private String telephone;
+    private Set<Pet> pets;
+
+    public String getAddrress() {
+        return addrress;
+    }
+
+    public void setAddrress(String addrress) {
+        this.addrress = addrress;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
 
     public Set<Pet> getPets() {
         return pets;
