a5a307ed349ef3614fa9867e949561c0326474b7
rayees <shaikhrayeesahmed@gmail.com>
2018-10-23 18:39:13 +0530

    Create speciality map service. closes #33

---
+++ pet-clinic-data/src/main/java/me/shaikhrayeesahmed/springpetclinic/services/SpaciatiltyService.java
@@ -0,0 +1,6 @@
+package me.shaikhrayeesahmed.springpetclinic.services;
+
+import me.shaikhrayeesahmed.springpetclinic.model.Speciality;
+
+public interface SpaciatiltyService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/me/shaikhrayeesahmed/springpetclinic/services/map/SpacialityMapService.java
@@ -0,0 +1,36 @@
+package me.shaikhrayeesahmed.springpetclinic.services.map;
+
+import me.shaikhrayeesahmed.springpetclinic.model.Speciality;
+import me.shaikhrayeesahmed.springpetclinic.services.SpaciatiltyService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpacialityMapService extends AbstractMapService<Speciality, Long> implements SpaciatiltyService {
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
